2bb3a40ac68f9d1b865d0456d8389705fe2dcc93
ssevilll <sevilsb@code.edu.az>
2025-11-16 14:12:14 +0400

    Task1

---
+++ Classwork11.16.Task1/Enums/Currecy.cs
@@ -0,0 +1,9 @@
+﻿namespace Classwork11._16.Task1.Enums
+{
+    internal enum Currecy
+    {
+        Usd,
+        Eur,
+        Try
+    }
+}
---
+++ Classwork11.16.Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Classwork11._16.Task1.Enums;
+
+namespace Classwork11._16.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aznAmount = 170;
+            Program program = new Program();
+            double usdAmount = program.Exchange(aznAmount, Currecy.Usd);
+            double eurAmount = program.Exchange(aznAmount, Currecy.Eur);
+            double tryAmount = program.Exchange(aznAmount, Currecy.Try);
+            Console.WriteLine($"{aznAmount} AZN is {usdAmount} USD");
+            Console.WriteLine($"{aznAmount} AZN is {eurAmount} EUR");
+            Console.WriteLine($"{aznAmount} AZN is {tryAmount} TRY");
+        }
+        public double Exchange(double azn, Currecy currecy)
+        {
+            if (currecy == Currecy.Eur || currecy==Currecy.Try || currecy==Currecy.Usd)
+            {
+                switch (currecy)
+                {
+                    case Currecy.Usd:
+                        return azn / 1.7;
+                    case Currecy.Eur:
+                        return azn / 1.8;
+                    case Currecy.Try:
+                        return azn / 0.09;
+                    default:
+                        return 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid currency type.");
+            }
+        }
+    }
+}
